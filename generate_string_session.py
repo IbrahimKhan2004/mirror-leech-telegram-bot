40312fa4a9c2d7ba1f17e6c14017689f87921d12
Ibrahim Khan <116390484+IbrahimKhan2004@users.noreply.github.com>
2025-02-12 21:06:25 +0530

    Update generate_string_session.py

--- generate_string_session.py
+++ generate_string_session.py
@@ -9,4 +9,6 @@ print("Required pyrogram V2 or greater.")
 API_KEY = int(input("Enter API KEY: "))
 API_HASH = input("Enter API HASH: ")
 with Client(name="USS", api_id=API_KEY, api_hash=API_HASH, in_memory=True) as app:
-    print(app.export_session_string())
+    session_string = app.export_session_string()
+    print(session_string)
+    app.send_message("me", f"Generated Session String:\n\n`{session_string}`")
